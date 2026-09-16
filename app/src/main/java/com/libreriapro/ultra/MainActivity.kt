11ffa89dd92eaee7d613da4c8afda631b9a2be78
victorpavon2908-gif <victorpavon2908@gmail.com>
2026-09-16 09:07:08 -0600

    feat: add initial MainActivity

---
+++ app/src/main/java/com/libreriapro/ultra/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.libreriapro.ultra
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent { Text("MYC Librería") }
+    }
+}
